b99b8513c225a5bd6839fe819297a79b5726b696
Rafael Geronimo <rafaelgeronimo@outlook.com>
2020-08-08 19:25:06 -0300

    baseurl apontando para heroku

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
-    //baseURL: 'https://proffy-server.herokuapp.com:3333',
+    /* baseURL: 'http://localhost:3333', */
+    baseURL: 'https://proffy-server.herokuapp.com/',
 });
 
 export default api;
